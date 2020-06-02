23b40615cdd33df51f8ae1d4b5e62440e4d540cb
Lucas Ariel <lucasarieln@outlook.com>
2020-06-01 23:14:21 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Projeto de Mapeamento Covid 19
+
+Projeto para mapear e interpretar os dados de Covid 19 consumindo uma API com ReactJS e PWA.
+
+Baseado na API ([https://coronavirus-19-api.herokuapp.com/countries](https://coronavirus-19-api.herokuapp.com/countries "https://coronavirus-19-api.herokuapp.com/countries")) estou consumindo os dados de diversos países e mostrando um dashboard com os principais dados para análise de cada país.
+
+
+Bibliotecas e tecnologias utilizadas:
+- Reactjs;
+- PWA;
+- Material UI;
+
+```javascript
+const headers = {
+  method: 'get',
+  mode: 'cors',
+  cache: 'default'
+}
+```
+
+
+![Dashboard](https://i.imgur.com/UFaF1Btr.png "Dashboard")
+
+
+
---
+++ src/commons/constants/countries.js
@@ -0,0 +1,47 @@
+import BrazilFlag from "../../assets/images/brazil.png";
+import ChinaFlag from "../../assets/images/china.png";
+import GermanyFlag from "../../assets/images/germany.png";
+import ItalyFlag from "../../assets/images/italy.png";
+import UsaFlag from "../../assets/images/usa.png";
+import SpainFlag from "../../assets/images/spain.png";
+import ArgentinaFlag from "../../assets/images/argentina.png";
+
+const COUNTRIES = [
+  {
+    label: "Alemanha",
+    value: "germany",
+    flag: GermanyFlag,
+  },
+  {
+    label: "Argentina",
+    value: "argentina",
+    flag: ArgentinaFlag,
+  },
+  {
+    label: "Brasil",
+    value: "brazil",
+    flag: BrazilFlag,
+  },
+  {
+    label: "Espanha",
+    value: "spain",
+    flag: SpainFlag,
+  },
+  {
+    label: "China",
+    value: "china",
+    flag: ChinaFlag,
+  },
+  {
+    label: "Estados Unidos",
+    value: "usa",
+    flag: UsaFlag,
+  },
+  {
+    label: "Itália",
+    value: "italy",
+    flag: ItalyFlag,
+  },
+];
+
+export default COUNTRIES;
